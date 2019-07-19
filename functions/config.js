6c89af7abd22f69a3377cf94828956e51fcddc3d
Neil T <neiltz711@gmail.com>
2019-07-18 20:17:48 -0400

    Update for public Viewing

--- functions/config.js
+++ functions/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: 'AIzaSyBuFxPHD4u2A8Y_FcCk1kpodOm8LfA4fTg',
-  authDomain: 'image-upload-9aebe.firebaseapp.com',
-  databaseURL: 'https://image-upload-9aebe.firebaseio.com',
-  projectId: 'image-upload-9aebe',
-  storageBucket: 'image-upload-9aebe.appspot.com',
-  messagingSenderId: '945989728811',
-  appId: '1:945989728811:web:32a3786769e1fe97'
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN',
+  databaseURL: 'DATABASE_URL',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'STORAGE_BUCKET',
+  messagingSenderId: 'MESSAGING_SENDER_ID',
+  appId: 'APP_ID'
 };
